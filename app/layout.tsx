5efa859811e143241f6cb5e38afe30719844a603
Noé Henchoz <henchoznoe@gmail.com>
2026-02-27 10:23:07 +0100

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import { cn } from '@/lib/utils/cn'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 interface LayoutProps {
@@ -58,6 +59,7 @@ const RootLayout = (props: Readonly<LayoutProps>) => {
         <ReactLenis root />
         <Toaster richColors position="bottom-right" />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
